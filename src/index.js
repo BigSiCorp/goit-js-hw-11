5401096aef4d14bf7c3a02ea9c43191d84808eba
BigSiCorp <si.kocorp@gmail.com>
2022-09-12 20:33:25 +0300

    initial2

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const a = 5;
